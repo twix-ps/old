aa3cd7e6348f423eb5705edb2bc838d63590445d
twix <59828082+twix-ps@users.noreply.github.com>
2023-11-11 16:31:29 +0200

    Fixed

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ function playSong() {
     songPlayer.load();
     songPlayer.play();
 }
+
+const topContainer = document.getElementById("top-container");
+const center = document.getElementById("center");
+let lastScrollPosition = 0;
+
+window.addEventListener("scroll", function() {
+  const scrollY = window.scrollY;
+
+  const scrollDirection = scrollY > lastScrollPosition ? "down" : "up";
+  lastScrollPosition = scrollY;
+
+  const translateY = scrollDirection === "down" ? -scrollY : 0;
+
+  topContainer.style.transform = `translateY(${translateY}px)`;
+  center.style.transform = `translateY(${translateY}px)`;
+});
